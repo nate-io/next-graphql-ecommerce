57a31c2c49393c78f19a9db9c9f929cca3f0069e
Nathaniel Young <nyoung125@gmail.com>
2021-02-15 16:09:28 -0500

    add page transition progress bar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+// customize transition progress bar styling
+import '../components/styles/nprogress.css';
+// attach progress bar to router page transition events
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
