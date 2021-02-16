83e7a53258f12ad4a3176133141c2a15b3d60f16
Nathaniel Young <nyoung125@gmail.com>
2021-02-16 11:35:08 -0500

    init keystone setup with remote db connection

---
+++ backend/keystone.ts
@@ -0,0 +1,31 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://locahost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long to stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseUrl,
+    // TODO: add data seeding to db prop
+  },
+  lists: createSchema({
+    // TODO: schema items here
+  }),
+  ui: {
+    // TODO: RBAC goes here
+    isAccessAllowed: () => true,
+  },
+});
